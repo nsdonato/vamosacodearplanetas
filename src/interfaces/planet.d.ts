306de4588dedde218975d71c21e8340e32dd6f4f
Michael Zabala (Mike) <wonkaphreak@gmail.com>
2022-09-09 11:44:36 -0400

    refactor(app): agrego un archivo de defincion para los tipos de response

---
+++ src/interfaces/planet.d.ts
@@ -0,0 +1,24 @@
+declare module "PlanetTypes" {
+  export interface ContentPlanet {
+    content: string;
+    source: string;
+  }
+
+  export interface ImagesPlanet {
+    planet: string;
+    internal: string;
+    geology: string;
+  }
+
+  export interface Planet {
+    name: string;
+    overview: ContentPlanet;
+    structure: ContentPlanet;
+    geology: ContentPlanet;
+    rotation: string;
+    revolution: string;
+    radius: string;
+    temperature: string;
+    images: ImagesPlanet;
+  }
+}
