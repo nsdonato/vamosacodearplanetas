91c18338c56bf1695c754ab5aa692758aa47debe
Michael Zabala(Mike) <wonkaphreak@gmail.com>
2022-09-20 19:32:45 -0400

    refactor(provider): agrego redirect de rutas 404 hacia un planeta en especifico

--- src/routers/AppRouter.routing.tsx
+++ src/routers/AppRouter.routing.tsx
@@ -1,22 +1,11 @@
-import { Route, useRoute } from "wouter";
+import { Route } from "wouter";
 import { Layout } from "../components/Layout";
-import { PlanetProvider } from "../context/PlanetProvider";
 
 export const AppRouter = () => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [_, params] = useRoute("/planets/:planetName");
-
   return (
     <>
       <Route path="/planets/:planetName">
-        <PlanetProvider planetName={params?.planetName}>
-          <Layout />
-        </PlanetProvider>
-      </Route>
-      <Route path="/">
-        <PlanetProvider>
-          <Layout />
-        </PlanetProvider>
+        <Layout />
       </Route>
     </>
   );
