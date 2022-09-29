2c22451671403db628e6e2cc764bff6adcea4978
nadiauj <nadiauj@ext.inditex.com>
2022-09-29 17:52:16 -0300

    feat(planetInfo): adapt estilos a figma

--- src/components/PlanetInfo.tsx
+++ src/components/PlanetInfo.tsx
@@ -6,16 +6,18 @@ export const PlanetInfo = () => {
   const { content, source } = planet?.overview;
 
   return (
-    <section className="flex flex-col p-2 gap-3 w-[310px]">
+    <section className="flex flex-col gap-6 w-[350px] h-[326px]">
       <h1 className="font-antonio text-80 uppercase text-white">{name}</h1>
-      <p className="font-spartan text-15 text-gray-light">{content}</p>
-      <span className="inline-flex font-spartan text-15 gap-1 text-gray-light">
+      <p className="font-spartan text-14 text-gray-light h-auto text-left align-top">
+        {content}
+      </p>
+      <span className="inline-flex font-spartan text-14 pt-1 pb-1 gap-1 w-[164px] h-[25px] text-gray-light">
         Source :
         <a
-          className="flex items-strech gap-2 underline font-bold text-gray-light"
+          className="flex flex-row gap-2 underline font-bold text-gray-light"
           href={source}
         >
-          Wikipedia<img className="self-center" src="../icon-source.svg"></img>
+          Wikipedia<img className="self-end" src="../icon-source.svg"></img>
         </a>
       </span>
     </section>
