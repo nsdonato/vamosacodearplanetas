7a394bde708674b829f0e8d5740ed6ef56cd3083
Michael Zabala(Mike) <wonkaphreak@gmail.com>
2022-10-06 10:47:56 -0400

    refactor(context): agrego validaciones para dispatch de ServicePlanet

--- src/context/PlanetProvider.tsx
+++ src/context/PlanetProvider.tsx
@@ -1,14 +1,9 @@
 import { useEffect, useReducer } from "react";
 import { PlanetContext } from "./PlanetContext";
-
-import { initialState, PlanetReducer } from "../reducer/PlanetReducer";
+import { PlanetReducer, initialState } from "../reducer/PlanetReducer";
 import { PlanetActions } from "../actions/PlanetActions";
 import { ServicePlanets } from "../service/planet";
-
-interface PlanetProviderProps {
-  children: React.ReactNode;
-  planetName?: string;
-}
+import { PlanetProviderProps } from "@planet/context";
 
 const PlanetProvider = ({
   planetName = "jupiter",
@@ -18,23 +13,22 @@ const PlanetProvider = ({
   const [state, dispatch] = useReducer(PlanetReducer, initialState);
 
   useEffect(() => {
-    void (async () => {
+    (async () => {
       dispatch({ type: PlanetActions.LOAD_PLANET });
-
       const res = await ServicePlanets.get(planetName);
       const { planet, error } = res;
-
+      if (error) {
+        dispatch({
+          type: PlanetActions.LOAD_PLANET_ERROR,
+          payload: error,
+        });
+      }
       if (planet) {
         dispatch({
           type: PlanetActions.LOAD_PLANET_SUCCESS,
           payload: planet,
         });
       }
-
-      dispatch({
-        type: PlanetActions.LOAD_PLANET_ERROR,
-        payload: error,
-      });
     })();
   }, [planetName]);
 
