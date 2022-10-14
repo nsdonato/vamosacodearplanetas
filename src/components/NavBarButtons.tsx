1cd4007ed417ab4cd1643e36f43b5366f86084ae
Christofer Castillo <ccastillo993a@gmail.com>
2022-10-13 21:50:18 -0500

    feat(menu): se añade transición al menu

--- src/components/NavBarButtons.tsx
+++ src/components/NavBarButtons.tsx
@@ -1,32 +1,43 @@
 import { Fragment } from "react";
 import { Link } from "wouter";
-import { Button, Img, Line } from "../components";
-import { ClassProps, MenuPlanetItem } from "@planet/components";
+import { ImageIcon, Line } from "../components";
+import { MenuPlanetItem } from "@planet/components";
 import { planets } from "../models";
 
-type NavButtonsProps = ClassProps;
+interface NavButtonsProps {
+  className: string;
+  hiddenMenu: () => void;
+}
 
-export const NavBarButtons = ({ className }: NavButtonsProps) => (
+export const NavBarButtons = ({ className, hiddenMenu }: NavButtonsProps) => (
   <nav className={className}>
-    <ul className="w-full">
+    <ul className="w-full mr-2">
       {planets.map((p: MenuPlanetItem) => {
         return (
           <Fragment key={p.name}>
-            <li className="py-4">
-              <Link href={`/planets/${p.name}`}>
-                <Button className="w-full p-4 flex items-center justify-between uppercase text-white font-spartan font-bold text-15 leading-25 tracking-1.36">
-                  <div className="flex">
-                    <Img
-                      src={`/oval-${p.name}.svg`}
-                      className="h-5 w-5 rounded-full mr-8"
-                    />
+            <li onClick={hiddenMenu} className="hover:bg-darkGray/20">
+              <Link
+                href={`/planets/${p.name}`}
+                className="w-full py-5 flex items-center uppercase justify-between"
+              >
+                <div className="flex items-center">
+                  <ImageIcon
+                    src={`/oval-${p.name}.svg`}
+                    className="h-5 w-5 rounded-full mr-6"
+                    altImage={p.name}
+                  />
+                  <h4 className="font-spartan text-white font-bold text-15 leading-25 tracking-1.36">
                     {p.name}
-                  </div>
-                  <Img src="/arrow-right.svg" className="h-3 w-3" />
-                </Button>
+                  </h4>
+                </div>
+                <ImageIcon
+                  src="/arrow-right.svg"
+                  className="h-3 w-3"
+                  altImage="arrow-menu"
+                />
               </Link>
             </li>
-            <Line className="w-11/12 my-0 m-auto" />
+            <Line className="last:hidden" />
           </Fragment>
         );
       })}
