d7ef877dfb2a91e0676a77241a2215dc5bea39d0
Christofer Castillo <ccastillo993a@gmail.com>
2022-10-13 22:08:35 -0500

    fix(menu): elimino la exportación adicional de ImageIcon

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,6 @@
 export * from "./Button";
 export * from "./ImageIcon";
 export * from "./Layout";
-export * from "./ImageIcon";
 export * from "./Line";
 export * from "./MenuPlanets";
 export * from "./NavBarButtons";
