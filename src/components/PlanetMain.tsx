39f38e10d836d093347b4e9718b38e608e38cf6b
Christofer Castillo <79947673+ccastillob@users.noreply.github.com>
2022-10-25 21:31:09 -0500

    refactor(planetMain): componente PlanetCardInfo dentro de section padre

--- src/components/PlanetMain.tsx
+++ src/components/PlanetMain.tsx
@@ -10,13 +10,11 @@ interface planetProps {
 
 export const PlanetMain = ({ planet }: planetProps) => {
   return (
-    <>
-      <section className="text-white grid grid-cols-12 gap-4 mt-[69px] md:mt-0 lg:gap-8 md:mx-6 lg:mx-8">
-        <PlanetTabs />
-        <PlanetImage img={"img"} imgAlt={"imgAlt"} />
-        <PlanetInfo />
-      </section>
+    <section className="text-white grid grid-cols-12 gap-4 gap-y-0 mt-[69px] md:gap-y-0 md:mt-0 md:mx-6 lg:gap-y-0 lg:gap-8 lg:mx-8">
+      <PlanetTabs />
+      <PlanetImage img={"img"} imgAlt={"imgAlt"} />
+      <PlanetInfo />
       <PlanetCardInfo />
-    </>
+    </section>
   );
 };
