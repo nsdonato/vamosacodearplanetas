e9b4887c851b80495a585662a8e828606fdfa3ec
Noelia Donato <nsdonatovignais@teco.com.ar>
2022-11-16 19:56:44 -0300

    fix: :bug: arregle la ruta para overview
    arregle la ruta de overview ya que faltaba el cambio en el navHeader

--- src/components/NavHeader.tsx
+++ src/components/NavHeader.tsx
@@ -23,7 +23,7 @@ export const NavHeader = ({ className = "" }: NavHeaderProps) => {
                       : "md:pt-2 lg:pt-[33px]"
                   } mx-4 first:ml-0 last:mr-0 font-spartan font-bold leading-25 tracking-1 lg:pb-[7px]`}
                 >
-                  <Link href={`/planets/${p.name}`}>
+                  <Link href={`/planets/${p.name}/overview`}>
                     <h4 className="text-11 text-white/75 uppercase hover:text-white cursor-pointer">
                       {p.name}
                     </h4>
