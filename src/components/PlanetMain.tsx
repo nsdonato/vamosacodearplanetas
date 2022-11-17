7ddd53bf9e538abcd331ebe43ba4d34b4f9163b6
Christofer Castillo <ccastillo993a@gmail.com>
2022-11-17 14:38:24 -0500

    feat(planetImage): añado vista de los planetas

--- src/components/PlanetMain.tsx
+++ src/components/PlanetMain.tsx
@@ -1,18 +1,13 @@
 import { PlanetImage } from "./PlanetImage";
 import { PlanetInfo } from "./PlanetInfo";
 import { PlanetCardInfo } from "./PlanetCardInfo";
-import { Planet } from "@planet/types";
 import { PlanetTabs } from "./PlanetTabs";
 
-interface planetProps {
-  planet?: Planet;
-}
-
-export const PlanetMain = ({ planet }: planetProps) => {
+export const PlanetMain = () => {
   return (
     <section className="text-white grid grid-cols-12 gap-4 gap-y-0 mt-[69px] md:gap-y-0 md:mt-0 md:mx-6 lg:gap-y-0 lg:gap-8 lg:mx-8">
       <PlanetTabs />
-      <PlanetImage img={"img"} imgAlt={"imgAlt"} />
+      <PlanetImage />
       <PlanetInfo />
       <PlanetCardInfo />
     </section>
