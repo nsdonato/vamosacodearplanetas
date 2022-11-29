d637cc39febd24498894b927cde304c47b8a87a9
noeliadv <noeliadv@ext.inditex.com>
2022-11-28 21:39:23 -0300

    feat(test): agrego comentarios y que corran los test post lint staged

--- src/tests/Button.test.tsx
+++ src/tests/Button.test.tsx
@@ -1,10 +1,18 @@
 import { render, screen } from "../utils/test-utils";
 import { Button } from "../components/Button";
 
+// TODO: h3 no es una etiqueta valida para estar dentro de un Button
+// Realizar tarea en Trello de mejoras y tenerlo en cuenta.
+
+// Test de ejemplo, sin terminar, pero corriendo ok
 it("should render", () => {
   render(
-    <Button name="hola" title={"Test"} characteristicName={"ke"} index={0} />
+    <Button
+      name="jupiter"
+      title={"jupiter"}
+      characteristicName={"jupiter"}
+      index={0}
+    />
   );
-
-  expect(screen.queryByText("Test")).toBeInTheDocument();
+  expect(screen.queryByText("jupiter")).toBeInTheDocument();
 });
