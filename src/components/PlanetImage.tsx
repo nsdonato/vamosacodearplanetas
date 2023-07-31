d1de98eb77420f9a0e3f3e71faa7452a24c4dbc0
Noelia Donato <noeliadonato@gmail.com>
2023-07-31 09:13:18 +0200

    refactor: improve images from png to webp or svg

--- src/components/PlanetImage.tsx
+++ src/components/PlanetImage.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
 import { usePlanet } from "../hooks";
 import { useRoute } from "wouter";
 
@@ -18,24 +19,36 @@ export const PlanetImage = () => {
     ([title, value]) => params?.characteristicName.includes(title) && value
   )[0];
 
+  const extWebp = ".webp";
+  const extSvg = ".svg";
+
   return (
     <div className="grid col-span-12 h-[260px] md:h-[426px] lg:col-span-7 lg:h-[670px] relative">
       <picture className="flex items-center justify-center">
-        <source srcSet={images.large} media="(min-width: 1024px)"></source>
-        <source srcSet={images.medium} media="(min-width: 768px)"></source>
-        <img src={images.small} alt={name} />
+        <source
+          srcSet={`${images.large}${extSvg}`}
+          media="(min-width: 1024px)"
+        ></source>
+        <source
+          srcSet={`${images.medium}${extSvg}`}
+          media="(min-width: 768px)"
+        ></source>
+        <img src={`${images.small}${extSvg}`} alt={name} />
       </picture>
       {characteristicName === "surface-geology" && (
         <picture className="absolute top-[75%] left-[50%] -translate-x-1/2 -translate-y-1/2">
           <source
-            srcSet={planetGeologyImages.large}
+            srcSet={`${planetGeologyImages.large}${extWebp}`}
             media="(min-width: 1024px)"
           ></source>
           <source
-            srcSet={planetGeologyImages.medium}
+            srcSet={`${planetGeologyImages.medium}${extWebp}`}
             media="(min-width: 768px)"
           ></source>
-          <img src={planetGeologyImages.small} alt={characteristicName} />
+          <img
+            src={`${planetGeologyImages.small}${extWebp}`}
+            alt={characteristicName}
+          />
         </picture>
       )}
     </div>
